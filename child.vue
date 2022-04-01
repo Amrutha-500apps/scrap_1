87f31fd4b219c1c2bbbea0faff76e69f6a9e1da6
Amrutha-500apps <102018614+Amrutha-500apps@users.noreply.github.com>
2022-04-01 10:36:28 +0530

    Add files via upload

---
+++ child.vue
@@ -0,0 +1,19 @@
+<template>
+    <div> 
+        <p>content:{{message}}</p>
+        <p>name:{{name}}</p>
+        <p>number:{{number}}</p>
+    </div>
+</template>
+<script>
+export default {
+    name:'childA2',
+    props :{
+        message:String,
+        name:String,
+        number:Number,
+        data:String,
+    }
+}
+</script>
+    
