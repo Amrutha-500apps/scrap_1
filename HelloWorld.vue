0dcc3a133b0589c1ef5bb5f04320bf0bbfc38856
Amrutha-500apps <102018614+Amrutha-500apps@users.noreply.github.com>
2022-04-01 10:35:59 +0530

    Add files via upload

--- HelloWorld.vue
+++ HelloWorld.vue
@@ -1,28 +1,34 @@
 <template>
   <div class="hello">
-    <h1>Hello Anusha</h1>
-    <p> Nice to meet you!.</p>
-    <bose></bose>
-  </div>
+  <child3 ref="message">{{message}}</child3>
+  <child message="hlo" name="amrutha" number=12345></child> 
+  <child1 :message="message" :name="name" :number="number"></child1>
+  <button @click="fun()">click me!</button>
+    </div>
 </template>
-
 <script>
-import bose from './demo.vue'
-export default {
-  name: 'HelloWorld',
-  components:{
-     bose,
-     
-  },
-
-
-  props: {
-
-  }
+import child from '@/components/child.vue'
+import child1 from '@/components/proj.vue'
+import child3 from '@/components/child3.vue'
+   export default{
+     name:'parentcomponentA2',
+        components:{
+          child,
+          child1,
+          child3,
+        },
+    data(){
+      return{
+        message:'hlo',
+        name:'amrutha',
+        number:'1234',
+        number1:'1264'
+      }
+    },
+   methods:{
+     fun(){
+       this.$ref.message.f1();
+     }
+   }
 }
 </script>
-
-
-<style scoped>
-
-</style>
