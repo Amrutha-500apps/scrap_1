b8c0ddcb19e616c8e0e5bff3fe210c17ae3293a6
Amrutha-500apps <102018614+Amrutha-500apps@users.noreply.github.com>
2022-04-01 10:27:42 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,12 +1,4 @@
-import Vue from 'vue'
+import { createApp } from 'vue'
 import App from './App.vue'
-import router from './router'
 
-Vue.config.productionTip = false
-
-let vueRoute = new Vue({
-  router,
-  render: h => h(App),
-}).$mount('#app')
-
-export default vueRoute
+createApp(App).mount('#app')
