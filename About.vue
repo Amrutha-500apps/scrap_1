60ed319f2df44b73f87f37ace78d47d4d317a6ff
Amrutha-500apps <102018614+Amrutha-500apps@users.noreply.github.com>
2022-04-01 10:15:52 +0530

    Add files via upload

---
+++ About.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="hello">
+    <h1> This is about vue routing</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AbouT',
+  props: {
+    msg: String
+  }
+}
+</script>
