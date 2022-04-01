ae99357d6397f49580d1b26cd4cc91c3cd15fa38
Amrutha-500apps <102018614+Amrutha-500apps@users.noreply.github.com>
2022-04-01 10:35:46 +0530

    Add files via upload

---
+++ proj.vue
@@ -0,0 +1,22 @@
+<template>
+    <div> 
+        <p>content:{{message}}</p>
+        <p>name:{{name}}</p>
+        <p>number:{{number}}</p>
+    </div>
+</template>
+<script>
+export default {
+    name:'childA3',
+    props :{
+        message:String,
+        name:String,
+        number:Number,
+    },
+     methods :{
+         f1(){
+         alert("successful")
+         }
+     }    
+}
+</script>
