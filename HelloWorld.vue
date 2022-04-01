7ba80d9eb6d0aa362cc09f38d4cbb4b79d6c7a2d
Amrutha-500apps <102018614+Amrutha-500apps@users.noreply.github.com>
2022-04-01 10:38:56 +0530

    Add files via upload

--- HelloWorld.vue
+++ HelloWorld.vue
@@ -1,34 +1,39 @@
 <template>
   <div class="hello">
-  <child3 ref="message">{{message}}</child3>
-  <child message="hlo" name="amrutha" number=12345></child> 
-  <child1 :message="message" :name="name" :number="number"></child1>
-  <button @click="fun()">click me!</button>
-    </div>
+    <h2> using props for static input</h2>
+    <ch message='hlo' name='amrutha' number=12345></ch>
+    <h2> using props for dynamic input</h2>
+    <ch :message='message1' :name='name1' :number='number1'></ch>
+    <h2> using ref</h2>
+    <ch msg="hlooo" ref="ref" message="hlo" name="amrutha" number="12345"></ch>
+    <button @click="fun()">click me</button>
+  </div>
 </template>
 <script>
-import child from '@/components/child.vue'
-import child1 from '@/components/proj.vue'
-import child3 from '@/components/child3.vue'
-   export default{
-     name:'parentcomponentA2',
-        components:{
-          child,
-          child1,
-          child3,
-        },
-    data(){
-      return{
-        message:'hlo',
-        name:'amrutha',
-        number:'1234',
-        number1:'1264'
+import ch from '@/components/ch.vue'
+export default {
+  name:'childsA2',
+  components :{
+    ch,
+  },
+  props:{
+    message:String,
+    name:String,
+    number:Number,
+  },
+      data(){
+       return{
+        message1:'hlo',
+        name1:'amrutha',
+        number1:'1234',
+        }
+      },
+      methods:{
+        fun(){
+          this.$refs.ref.f1();
+        }
       }
-    },
-   methods:{
-     fun(){
-       this.$ref.message.f1();
-     }
-   }
 }
 </script>
+
+    
