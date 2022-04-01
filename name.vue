504907c5eff2949fd0536f2914c88bc6c165656d
Amrutha-500apps <102018614+Amrutha-500apps@users.noreply.github.com>
2022-04-01 10:17:15 +0530

    Add files via upload

---
+++ name.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <p>{{message}}
+    </div>
+</template>
+<script>
+export default {
+    name:'hlo', 
+    props:{
+        name:String,
+    },  
+    data(){
+        return{
+            name:"mantra"
+        }
+    },
+      methods: {
+        change_name(){
+            this.name = "agile";
+        }
+    }  
+    
+}
+</script>
