b2d98a41eac6d925bb86d2252b64c66d0633c3bd
Amrutha-500apps <102018614+Amrutha-500apps@users.noreply.github.com>
2022-04-01 10:36:17 +0530

    Add files via upload

---
+++ child3.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <p>msg:{{message}}</p>
+    </div>
+</template>
+<script>
+     export default{
+         name:'child4A3',
+         props:{
+             message:{default:'amrutha',type:String},
+            }
+        }
+</script>
+
