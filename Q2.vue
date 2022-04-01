b2cb68aa5befd9333527ebeeda1f9263f2a8d67a
Amrutha-500apps <102018614+Amrutha-500apps@users.noreply.github.com>
2022-04-01 10:06:56 +0530

    Add files via upload

---
+++ Q2.vue
@@ -0,0 +1,72 @@
+<template>
+
+<div>
+ <h1>Question2</h1>   
+
+<center><table>
+
+<thead>
+
+<tr>
+
+<th></th>
+
+</tr>
+
+</thead>
+
+<tbody>
+
+<tr v-for="data in table_data" :key="data.id">
+
+<td>Name :{{data.name}}</td>
+
+<td>Description : {{data.description}}</td>
+
+<td>Data_Of_Experience : {{data.year_of_experience}}</td>
+
+<td>Location :{{data.location}}</td>
+
+</tr>
+
+</tbody>
+
+</table></center>
+
+</div>
+
+</template>
+
+
+<script>
+
+export default {
+
+name:"displayData",
+
+data()
+
+{
+
+return{
+
+table_data:[{
+
+"name":"Agile CRM",
+
+"description" : "A company of excellence",
+
+"year_of_experience" : 12,
+
+"location" : "Hyderabad"
+
+}]
+
+}
+
+}
+
+}
+
+</script>
+
