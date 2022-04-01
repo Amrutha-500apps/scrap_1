6e187928726498d64f754644f7ed17367829dc01
Amrutha-500apps <102018614+Amrutha-500apps@users.noreply.github.com>
2022-04-01 10:32:28 +0530

    Add files via upload

---
+++ demo.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <p><b>This My First Project</b></p>
+        </div>
+</template>
+<script>
+export default{
+    name:'deY112',
+}
+</script>
+<style scoped>
+
+</style>
