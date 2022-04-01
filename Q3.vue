bfc65bc01377b6526fcccba5b9c35c6aeff86f96
Amrutha-500apps <102018614+Amrutha-500apps@users.noreply.github.com>
2022-04-01 10:07:19 +0530

    Add files via upload

---
+++ Q3.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <p>local variables</p>
+    </div>
+</template>
+<script>
+import child2 from './components/child2'
+export default{
+    name:'local',
+    components :{
+        child2,
+    },
+    props:{
+        name:String,
+    }
+}
+</script>
