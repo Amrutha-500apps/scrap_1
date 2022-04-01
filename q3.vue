6fd9f9ddb2b07491e3576dbf0b2de1406f0f4b46
Amrutha-500apps <102018614+Amrutha-500apps@users.noreply.github.com>
2022-04-01 10:07:39 +0530

    Add files via upload

---
+++ q3.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <p>{{name}}</p>
+    </div>
+</template>
+<script>
+import reg from '@/components/reg'
+export default{
+    name:'hloA3',
+    components :{
+        reg,
+    },
+    props: {
+        name:String
+    }
+}
+</script>
